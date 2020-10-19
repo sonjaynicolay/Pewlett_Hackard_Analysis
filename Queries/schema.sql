4aa68e976addb202a86556b2a0ffde8481e5d324
Sonjay Nicolay <sonjaynicolay@gmail.com>
2020-10-19 00:08:17 -0500

    Final Commit

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,58 +1,43 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
-);
+Departments
+-
+dept_no varchar pk
+dept_name varchar
 
-CREATE TABLE employees (
-	 emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
-);
+Employees
+-
+emp_no int pk
+birth_date date
+first_name varchar
+last_name varchar
+gender varchar
+hire_date date
 
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
+Managers
+-
+dept_no varchar pk fk - Departments.dept_no
+emp_no int fk - Employees.emp_no
+from_date date
+to_date date
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+Dept_Emp
+-
+dept_no varchar pk fk >- Departments.dept_no
+emp_no int pk fk >- Employees.emp_no
+from_date date
+to_date date
 
-);
+Titles
+-
+emp_no int pk fk >- Employees.emp_no
+# Adds pk restraint to title column
+title varchar pk
+# Adds pk restraint to from_date column
+from_date date pk
+to_date date
 
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	--Adds pk restraint to from_date column
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title, from_date)
-	
-);
-
-CREATE TABLE salaries (
-  	emp_no INT NOT NULL,
-  	salary INT NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
-);
+Salaries
+-
+emp_no int pk fk - Employees.emp_no
+salary int
+from_date date
+to_date date
